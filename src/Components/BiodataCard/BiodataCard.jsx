298c954d1278c1296c2bae80ad39413867f0fe5f
Rakesh Biswas <rbiswas01999@gmail.com>
2024-06-10 12:15:33 +0600

    Biodata-card design added

--- src/Components/BiodataCard/BiodataCard.jsx
+++ src/Components/BiodataCard/BiodataCard.jsx
@@ -10,31 +10,35 @@ const BiodataCard = ({ biodata }) => {
     }
 
     return (
-        <Card className="max-w-full rounded-lg shadow-2xl mb-5 p-4 flex flex-col items-center">
+        // <Card className="max-w-full rounded-lg shadow-2xl mb-5 p-4 flex flex-col items-center">
+        <Card className="hover:shadow-2xl transition duration-300 ease-in-out p-4 hover:bg-blue-200" sx={{ backgroundColor: 'ButtonHighlight' }}>
             <CardMedia
                 component="img"
                 alt="biodata"
                 image={ProfileImage}
-                className="mt-5"
+                className="mt-5 "
                 style={{ height: '200px', objectFit: 'cover' }} // Set fixed height and object-fit
             />
-            <Typography variant="body2" color="textSecondary" className="mt-2 bg-blue-200 text-black px-2">
-                Age: {Age}
-            </Typography>
-            <CardContent className="flex flex-col items-center gap-2">
-                <Typography variant="h6" component="div">
+            <div className='flex justify-center '>
+                <Typography variant="body2" color="textSecondary" className="mt-2 w-1/2 bg-blue-300 text-black text-center ">
+                    <span className='md:text-xl'>Age: {Age}</span>
+                </Typography>
+            </div>
+            <CardContent className="flex flex-col items-center gap-2 ">
+                <Typography variant="h5" component="div">
                     {name}
                 </Typography>
-                <Typography variant="body2" color="textSecondary">
+                {/* <Typography variant="body1" color="textSecondary"> */}
+                <Typography variant="body1" color="textPrimary">
                     BiodataId: {BiodataId}
                 </Typography>
-                <Typography variant="body2" color="textSecondary">
+                <Typography variant="body1" color="textPrimary">
                     {PermanentDivision}
                 </Typography>
-                <Typography variant="body2" color="textSecondary">
+                <Typography variant="body1" color="blue">
                     {BiodataType}
                 </Typography>
-                <Typography variant="body2" color="textSecondary">
+                <Typography variant="body1" color="">
                     {Occupation}
                 </Typography>
                 <Link to={`/biodatas/${_id}`} className="w-full">
